3d730e80d376f7818e4426b41bd2687476bad23e
destocot <khurramcali@gmail.com>
2023-10-10 21:21:15 -0400

    edited routing

--- app/(auth)/landing/page.tsx
+++ app/(auth)/landing/page.tsx
@@ -1,39 +1,26 @@
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-// import Image from "next/image";
 import Link from "next/link";
-import { cookies } from 'next/headers'
-import { redirect } from "next/navigation";
 import LandingVisual from "@/components/LandingVisual";
 import GoogleLogin from "./GoogleLogin";
 
+const greetings = [
+  'Hello World',         // English
+  'Hola Mundo',          // Spanish
+  'Bonjour le Monde',   // French
+  'こんにちは、世界',    // Japanese
+  '你好，世界',           // Chinese
+  '안녕하세요, 세계',    // Korean
+  'ہیلو دنیا',         // Urdu
+  'Ciao Mondo',          // Italian
+  'مرحبًا بالعالم',    // Arabic
+  'Привет, мир',         // Russian
+  'Hallo Welt'           // German
+];
+
 export default async function LandingPage() {
-  // redirect user if they have a session (logged in)
-  const supabase = createServerComponentClient({ cookies })
-  const { data } = await supabase.auth.getSession()
-
-  if (data.session) {
-    redirect('/')
-  }
-
-  const greetings = [
-    'Hello World',         // English
-    'Hola Mundo',          // Spanish
-    'Bonjour le Monde',   // French
-    'こんにちは、世界',    // Japanese
-    '你好，世界',           // Chinese
-    '안녕하세요, 세계',    // Korean
-    'ہیلو دنیا',         // Urdu
-    'Ciao Mondo',          // Italian
-    'مرحبًا بالعالم',    // Arabic
-    'Привет, мир',         // Russian
-    'Hallo Welt'           // German
-  ];
-
   return (
     <main className="h-screen flex justify-center items-center my-0">
       <div>
         <h1 className="uppercase text-8xl bg-light-three/75 -skew-x-6 font-bold w-fit mx-auto mb-2 px-4 dark:border-dark-four dark:bg-dark-five/50">stick-it!</h1>
-        {/* <Image src="https://picsum.photos/id/8/300" alt="" width={300} height={300} /> */}
         <div className="relative -z-10">
           <LandingVisual />
           <code className="text-dark-one font-semibold absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">{greetings[Math.floor(Math.random() * greetings.length)]}</code>
