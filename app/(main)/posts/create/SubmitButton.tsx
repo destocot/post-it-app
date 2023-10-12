2a6ae87b002807d208b33eecf3a29e6d5e019788
destocot <khurramcali@gmail.com>
2023-10-12 04:06:10 -0400

    removed toasts

--- app/(main)/posts/create/SubmitButton.tsx
+++ app/(main)/posts/create/SubmitButton.tsx
@@ -1,8 +1,5 @@
 "use client"
-import { usePathname, useSearchParams } from "next/navigation";
-import { MouseEvent, useEffect, useState } from "react";
-import toast, { Toaster } from "react-hot-toast";
-
+import { MouseEvent, useState } from "react";
 export default function SubmitButton() {
   const [disabled, setDisabled] = useState(false);
 
@@ -11,19 +8,8 @@ export default function SubmitButton() {
     new Promise(() => setTimeout(() => setDisabled(false), 5000));
   }
 
-  const pathname = usePathname();
-  const endPath = pathname.split("/").pop();
-  useEffect(() => {
-    if (endPath === "edit") {
-      toast("editing ✏️")
-    } else if (endPath === "create") {
-      toast("creating ✏️")
-    }
-  }, [endPath])
-
   return (
     <>
-      <Toaster />
       <button
         id="create-post-button"
         className="ml-auto py-1 px-2 rounded-md transition-all bg-purple-two hover:scale-110 text-white" disabled={disabled} onClick={(e) => clickHandler(e)} type="submit">Submit</button>
