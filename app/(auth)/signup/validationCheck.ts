9245cff025eeaaa1c0809149e1e5a1f74659f53f
destocot <khurramcali@gmail.com>
2023-10-13 01:49:01 -0400

    vercel fix 1

--- app/(auth)/signup/validationCheck.ts
+++ app/(auth)/signup/validationCheck.ts
@@ -1,6 +1,3 @@
-import { Database } from "@/types/database.types";
-import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
-
 export const validationCheck = async (signupData: {
   name: string;
   email: string;
